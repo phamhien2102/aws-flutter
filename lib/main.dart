deae68e0252a464bb906b29ecdc2968e8462389b
hien.p <hien.p@autonomous.nyc>
2021-06-27 01:38:37 +0700

    Week 1 - Change theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: RandomWords(),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+    );
   }
 }
 
